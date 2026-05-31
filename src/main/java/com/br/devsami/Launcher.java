89bfadd50815be7e5a72191d0258a29f52e9ba72
Francisco Samily <francisco.samily1@gmail.com>
2026-05-31 17:55:03 -0300

    Adiciona classe responsavel por inicar o programa

---
+++ src/main/java/com/br/devsami/Launcher.java
@@ -0,0 +1,13 @@
+package com.br.devsami;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            com.br.devsami.utils.HibernateUtil.close();
+        })); // fechar o h2
+        Application.launch(App.class, args);
+    }
+
+}
