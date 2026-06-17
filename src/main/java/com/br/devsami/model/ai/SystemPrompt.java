04721051c65ecd57ac72b7a61f4894ef0eff546a
Francisco Samily <francisco.samily1@gmail.com>
2026-06-17 13:58:22 -0300

    chore: adiciona camada inicial de suporte para as funcionalidades de IA

---
+++ src/main/java/com/br/devsami/model/ai/SystemPrompt.java
@@ -0,0 +1,6 @@
+package com.br.devsami.model.ai;
+
+
+// Esse lugar dsefine o comportamento da do agente, como ele deve agir, se conportar os valores dele, as limitações explicitas, a explicação minima operacional que ele precisa saber.
+public class SystemPrompt {
+}
