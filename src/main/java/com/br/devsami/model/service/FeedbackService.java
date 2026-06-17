4f4d274d4640866c778c70c6bbb5ddc134053aa3
Francisco Samily <francisco.samily1@gmail.com>
2026-06-17 18:38:41 -0300

    refactor: coloca a analise de confiaca pela IA

--- src/main/java/com/br/devsami/model/service/FeedbackService.java
+++ src/main/java/com/br/devsami/model/service/FeedbackService.java
@@ -1,5 +1,6 @@
 package com.br.devsami.model.service;
 
+import com.br.devsami.model.ai.AiOrchestratorService;
 import com.br.devsami.model.entity.Employee;
 import com.br.devsami.model.entity.Feedback;
 import com.br.devsami.model.entity.User;
@@ -10,9 +11,11 @@ import com.br.devsami.utils.enums.Feelling;
 public class FeedbackService {
 
     private final FeedbackRepository feedbackRepository;
+    private final AiOrchestratorService aiService;
 
     public FeedbackService() {
         this.feedbackRepository = new FeedbackRepository();
+        this.aiService = new AiOrchestratorService();
     }
 
     public Feedback createFeedback(
@@ -22,30 +25,35 @@ public class FeedbackService {
             FeedbackCategory category,
             String text) {
 
-        if (user == null) {
-            throw new IllegalArgumentException("User obrigatório");
-        }
-
-        if (employee == null) {
-            throw new IllegalArgumentException("Employee obrigatório");
+        if (user == null || employee == null) {
+            throw new IllegalArgumentException("User e Employee são obrigatórios");
         }
 
         Feedback feedback = new Feedback();
         feedback.setUser(user);
         feedback.setEmployee(employee);
-        feedback.setFeelling(feeling);
         feedback.setCategory(category);
         feedback.setText(text);
 
-        // regras simples iniciais
         if (text == null || text.isBlank()) {
+            feedback.setFeelling(feeling);
             feedback.setConfidence(100);
             feedback.setSarcasmDetected(false);
-            feedback.setReasoning("Feedback sem texto (voto direto)");
+            feedback.setReasoning("Voto direto sem texto");
+        } else {
+            try {
+                String inputIa = "Original Feeling: " + feeling.name() + "\nText: " + text;
+                String iaResponse = aiService.processMessage(inputIa).trim();
+
+                feedback.setFeelling(Feelling.valueOf(iaResponse));
+                feedback.setReasoning("Analisado via IA");
+            } catch (Exception e) {
+                feedback.setFeelling(feeling);
+                feedback.setReasoning("Fallback: Erro na IA. Voto original mantido.");
+            }
         }
 
         feedbackRepository.save(feedback);
-
         return feedback;
     }
 }
