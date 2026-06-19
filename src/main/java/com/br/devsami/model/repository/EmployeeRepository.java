68729e47cde496fe5d40df9a34ab1c0e04cb3772
Francisco Samily <francisco.samily1@gmail.com>
2026-06-19 16:23:28 -0300

    fix: Corrige bug da versao do lombok

--- src/main/java/com/br/devsami/model/repository/EmployeeRepository.java
+++ src/main/java/com/br/devsami/model/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import com.br.devsami.model.entity.Employee;
 import com.br.devsami.utils.HibernateUtil;
+import jakarta.persistence.EntityManager;
 
 import java.util.List;
 import java.util.Optional;
