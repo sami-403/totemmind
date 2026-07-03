8d216c3421e3236234feb9efef542f161753bddd
sami-403 <francisco.samily1@gmail.com>
2026-07-03 06:54:20 -0300

    feat: Adiciona telas referente ao feedback

---
+++ src/main/java/com/br/devsami/controller/AvaliacaoController.java
@@ -0,0 +1,64 @@
+package com.br.devsami.controller;
+
+import com.br.devsami.model.repository.UserRepository;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class AvaliacaoController {
+
+    @FXML
+    private TextField txtCpf;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    void voltar(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/MenuPrincipal.fxml")));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root, 800, 600));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void seguirParaAvaliacao(ActionEvent event) {
+        String cpf = txtCpf.getText();
+        if (cpf.isBlank()){
+            return;
+        }
+
+        UserRepository userRepository = new UserRepository();
+
+        if(userRepository.existsByCpf(cpf)){
+
+            try {
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FeedbackMenu.fxml")));
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(new Scene(root, 800, 600));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Fluxo:
+        // 1. Validar CPF
+        // 2. userRepository.existsByCpf(cpf)
+        // 3. Se existe -> Tela de Escolha entre Funcionário e produtos (Avaliação)
+        // 4. Se não existe -> Tela de Cadastro de Usuário
+    }
+
+}
