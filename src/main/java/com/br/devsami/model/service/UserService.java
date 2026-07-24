8c7acf412ba3df199cea95c0beb1d2a6a704669b
Francisco Everton Maciel Rodrigues <macielyu@gmail.com>
2026-07-24 18:00:02 -0300

    feat: Acrescenta método de edição de clientes ao UserService

--- src/main/java/com/br/devsami/model/service/UserService.java
+++ src/main/java/com/br/devsami/model/service/UserService.java
@@ -3,6 +3,7 @@ package com.br.devsami.model.service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import com.br.devsami.model.entity.User;
 import com.br.devsami.model.repository.UserRepository;
@@ -44,7 +45,7 @@ public class UserService {
         return (int) Math.ceil((double) entriesCount / pageSize);
     }
 
-    // 1. Buscar usuário
+    // Buscar usuário
     public Optional<User> findByCpf(String cpf) {
         if (cpf.isEmpty() || CpfValidator.isValidCpf(cpf)){
             throw new IllegalArgumentException(CpfValidator.validate(cpf));
@@ -53,7 +54,7 @@ public class UserService {
         return userRepository.findByCpf(cpf);
     }
 
-    // 2. Criar usuário
+    // Criar usuário
     public User createUser(String name, String cpf, LocalDate birthDate) {
 
         if (name == null || name.isBlank()) {
@@ -87,7 +88,41 @@ public class UserService {
         return user;
     }
 
-    // 3. Fluxo principal do totem (ESSA É A PARTE MAIS IMPORTANTE)
+    // Editar um usuário
+    public User editUser(UUID id, String name, String cpf, LocalDate birthDate) {
+        var user = userRepository.findById(id).orElseThrow();
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Nome obrigatório");
+        }
+
+        if (cpf == null || cpf.isBlank()) {
+            throw new IllegalArgumentException("CPF obrigatório");
+        }
+
+        String validationError = CpfValidator.validate(cpf);
+        if (validationError != null) {
+            throw new IllegalArgumentException(validationError);
+        }
+
+        if (birthDate == null) {
+            throw new IllegalArgumentException("Data de nascimento obrigatória");
+        }
+
+        if (userRepository.existsByCpf(cpf) && !user.getCpf().equals(cpf.strip())) {
+            throw new IllegalArgumentException("CPF já cadastrado");
+        }
+
+        user.setName(name);
+        user.setCpf(cpf);
+        user.setBirthDate(birthDate);
+
+        userRepository.update(user);
+
+        return user;
+    }
+
+    // Fluxo principal do totem (ESSA É A PARTE MAIS IMPORTANTE)
     public User findOrCreateUser(String name, String cpf, LocalDate birthDate) {
 
         Optional<User> existing = userRepository.findByCpf(cpf);
